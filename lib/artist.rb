ce3c7c5c01c43773fde741ad290cdc93e7ba540b
Laura Trager <laura.trager@gmail.com>
2017-11-22 18:50:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,23 @@
 class Artist
 
-  attr_accessor :song_name, :song
+  attr_accessor :name, :song
 
   def initialize(name)
     @name = name
     @songs = []
   end
 
-  def add_song(song_name)
-    @song_name = song_name
-    @song_name.artist = self
-    @songs << @song_name
-    @song_name
+  def add_song(song)
+    @song = song
+    @song.artist = self
+    @songs << @song
+    @song
   end
 
-  def add_song_by_name(song_name)
-    @song_name = Song.new(song_name)
-    @song_name.artist = self
-    @songs << @song_name
+  def add_song_by_name(name)
+    @song = Song.new(name)
+    @song.artist = self
+    @songs << @song
   end
 
   def songs
